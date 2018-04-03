a0894336663ef6988318043beaf56cd973f762f0
Yuliya Pinchuk <ypinchuk@uncc.edu>
2018-04-03 01:53:55 +0000

    Add Adventures index method and view aka My Saved Adventures

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -1,4 +1,8 @@
 class AdventuresController < ApplicationController
+    def index
+        @adventures = Adventure.all
+    end
+    
     def show
         @adventure = Adventure.find(params[:id])
     end
