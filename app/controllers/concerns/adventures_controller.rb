deaeb47b98bd916d4d26d81e45593d49fd40a0ef
Yuliya Pinchuk <ypinchuk@uncc.edu>
2018-04-11 19:09:19 +0000

    Saved files

---
+++ app/controllers/concerns/adventures_controller.rb
@@ -0,0 +1,28 @@
+class AdventuresController < ApplicationController
+    def index
+        @adventures = Adventure.all
+    end
+    
+    def show
+        @adventure = Adventure.find(params[:id])
+    end
+    
+    def new
+        @adventure = Adventure.new
+    end
+    
+    def create
+        @adventure = Adventure.new(adventure_params)
+        
+        if @adventure.save
+            redirect_to @adventure
+        else
+            render 'new'
+        end
+    end
+    
+    private
+        def adventure_params
+            params.require(:adventure).permit(:name)
+        end
+end
