15d029b62668aa44cb4767222aca2ef7a52745d7
Yuliya Pinchuk <ypinchuk@uncc.edu>
2018-04-14 01:12:26 +0000

    Add create functionality to Storyline

--- app/controllers/storylines_controller.rb
+++ app/controllers/storylines_controller.rb
@@ -1,13 +1,13 @@
 class StorylinesController < ApplicationController
     def create
-        #@adventure = Adventure.find(params[:adventure_id])
-        #@storyline = @adventure.storyline.create(storyline_params)
-        @storyline = Storyline.new(:adventure_id => 2)
+        @adventure = Adventure.find(params[:adventure_id])
+        @storyline = @adventure.storylines.create(storyline_params)
+        #@storyline = Storyline.new(:adventure_id => 2)
         redirect_to adventure_path(@adventure)
     end
     
     private
-    def comment_params
-      params.require(:storyline).permit(:storyline)
-    end
+        def storyline_params
+            params.require(:storyline).permit(:selection)
+        end
 end
