91151cf701a1774e66ffb4a7c2ebe7ccf9122da9
Yuliya Pinchuk <ypinchuk@uncc.edu>
2018-04-21 02:25:26 +0000

    Add user adds an adventure scenario 2 - Radio button failing

--- spec/integration/user_adds_an_adventure_spec.rb
+++ spec/integration/user_adds_an_adventure_spec.rb
@@ -7,8 +7,22 @@ feature "User adds an adventure" do
         click_link "New Adventure"
         expect(page).to have_content("New Adventure")
         expect(page).to have_content("Enter a name for your new adventure!")
-        expect(page).to have_content("Name")
-        #expect(page).to have_field("Name")
+        #expect(page).to have_content("Name")
+        expect(page).to have_field("Name") 
         expect(page).to have_content("Storyline")
     end
+    scenario "User successfully creates a new adventure" do
+        visit new_adventure_path
+        expect(page).to have_content("New Adventure")
+        fill_in "Name", with: "New Adventure"
+        choose('Storyline 1')
+        #find('[name=story1]').click
+        click_button "Create Adventure"
+        expect(page).to have_content("New Adventure")
+        expect(page).to have_content("Storyline 1")
+    end
+end
+
+feature "User deletes an adventure" do
+    
 end
