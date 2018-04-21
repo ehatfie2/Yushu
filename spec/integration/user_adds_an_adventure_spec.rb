3b7eb7185d3ebb42b620e78cb451dca186ef236d
Yuliya Pinchuk <ypinchuk@uncc.edu>
2018-04-21 03:55:45 +0000

    Add User deletes an adventure feature

--- spec/integration/user_adds_an_adventure_spec.rb
+++ spec/integration/user_adds_an_adventure_spec.rb
@@ -15,8 +15,9 @@ feature "User adds an adventure" do
         visit new_adventure_path
         expect(page).to have_content("New Adventure")
         fill_in "Name", with: "New Adventure"
-        choose('Storyline 1')
-        #find('[name=story1]').click
+        #choose('Storyline 1')
+        expect(page).to have_content("Choose a storyline for your adventure")
+        find('story1').click
         click_button "Create Adventure"
         expect(page).to have_content("New Adventure")
         expect(page).to have_content("Storyline 1")
@@ -24,5 +25,21 @@ feature "User adds an adventure" do
 end
 
 feature "User deletes an adventure" do
-    
+    scenario "User successfully creates a new adventure" do
+        visit new_adventure_path
+        expect(page).to have_content("New Adventure")
+        fill_in "Name", with: "New Adventure"
+        page.choose('Storyline 1')
+        #find('[name=story1]').click
+        click_button "Create Adventure"
+        expect(page).to have_content("New Adventure")
+        expect(page).to have_content("Storyline 1")
+    end
+    scenario "User succesfully deletes an adventure from the adventure index page" do
+        visit adventures_path
+        expect(page).to have_content("Saved Adventures")
+        expect(page).to have_content("New Adventure")
+        click_link "Delete"
+        expect(page).to have_no_content('New Adventure')
+    end
 end
