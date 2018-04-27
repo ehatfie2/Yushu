7f42fff12e562f5e95e57c88cb2a219d011c8d47
Yuliya Pinchuk <ypinchuk@uncc.edu>
2018-04-27 23:27:38 +0000

    Arranged play radio buttons in a list

--- spec/integration/user_adds_an_adventure_spec.rb
+++ spec/integration/user_adds_an_adventure_spec.rb
@@ -8,13 +8,13 @@ feature "User adds an adventure" do
         expect(page).to have_content("New Adventure")
         expect(page).to have_content("Enter a name for your new adventure!")
         #expect(page).to have_content("Name")
-        expect(page).to have_field("Name") 
+        expect(page).to have_field('name') 
         expect(page).to have_content("Storyline")
     end
     scenario "User successfully creates a new adventure" do
         visit new_adventure_path
         expect(page).to have_content("New Adventure")
-        fill_in "Name", with: "New Adventure"
+        fill_in 'name', with: "New Adventure"
         expect(page).to have_content("Choose a storyline for your adventure")
         #choose("Storyline 1")
         expect(page).to have_content("Storyline 1")
@@ -29,7 +29,7 @@ feature "User deletes an adventure" do
     scenario "User successfully creates a new adventure" do
         visit new_adventure_path
         expect(page).to have_content("New Adventure")
-        fill_in "Name", with: "New Adventure"
+        fill_in 'name', with: "New Adventure"
         #page.choose("Storyline 1")
         #find('[name=story1]').click
         expect(page).to have_content("Storyline 1")
@@ -41,7 +41,7 @@ feature "User deletes an adventure" do
         visit adventures_path
         expect(page).to have_content("Saved Adventures")
         expect(page).to have_content("New Adventure")
-        click_link "Delete"
+        find('New Adventure', text: name).click_link('delete')
         expect(page).to have_no_content('New Adventure')
     end
 end
