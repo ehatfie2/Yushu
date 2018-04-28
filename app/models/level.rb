a597662b27a64c83909776f67152304e8c3bfe76
Yuliya Pinchuk <ypinchuk@uncc.edu>
2018-04-28 15:47:22 +0000

    Added loop to radio buttons in play to pull from user_choices hash

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,3 +1,11 @@
 class Level < ApplicationRecord
   belongs_to :adventure
+  
+    USER_CHOICES = {
+	"1_1" => "You go home",
+	"1_2" => "You investigate",
+	"1_3" => "TEXT HERE",
+	"2_1_1" => "TEXT HERE",
+    }
+  
 end
